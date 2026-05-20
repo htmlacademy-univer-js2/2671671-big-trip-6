874cf82a83d034fd9059fefcda568f79bd26e171
Keks <keks@htmlacademy.ru>
2026-05-20 15:07:26 +0300

    :cat:

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,5 +1,6 @@
 import AbstractView from './view/abstract-view.js';
 
+/** @enum {string} Перечисление возможных позиций для отрисовки */
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
@@ -7,6 +8,11 @@ const RenderPosition = {
   AFTEREND: 'afterend',
 };
 
+/**
+ * Функция для создания элемента на основе разметки
+ * @param {string} template Разметка в виде строки
+ * @returns {HTMLElement} Созданный элемент
+ */
 function createElement(template) {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
@@ -14,6 +20,12 @@ function createElement(template) {
   return newElement.firstElementChild;
 }
 
+/**
+ * Функция для отрисовки элемента
+ * @param {AbstractView} component Компонент, который должен был отрисован
+ * @param {HTMLElement} container Элемент в котором будет отрисован компонент
+ * @param {string} place Позиция компонента относительно контейнера. По умолчанию - `beforeend`
+ */
 function render(component, container, place = RenderPosition.BEFOREEND) {
   if (!(component instanceof AbstractView)) {
     throw new Error('Can render only components');
@@ -26,6 +38,11 @@ function render(component, container, place = RenderPosition.BEFOREEND) {
   container.insertAdjacentElement(place, component.element);
 }
 
+/**
+ * Функция для замены одного компонента на другой
+ * @param {AbstractView} newComponent Компонент, который нужно показать
+ * @param {AbstractView} oldComponent Компонент, который нужно скрыть
+ */
 function replace(newComponent, oldComponent) {
   if (!(newComponent instanceof AbstractView && oldComponent instanceof AbstractView)) {
     throw new Error('Can replace only components');
@@ -43,6 +60,10 @@ function replace(newComponent, oldComponent) {
   parent.replaceChild(newElement, oldElement);
 }
 
+/**
+ * Функция для удаления компонента
+ * @param {AbstractView} component Компонент, который нужно удалить
+ */
 function remove(component) {
   if (component === null) {
     return;
--- src/framework/ui-blocker/ui-blocker.js
+++ src/framework/ui-blocker/ui-blocker.js
@@ -1,13 +1,32 @@
 import './ui-blocker.css';
 
+/**
+ * Класс для блокировки интерфейса
+ */
 export default class UiBlocker {
+  /** @type {number} Время до блокировки интерфейса в миллисекундах */
   #lowerLimit;
+
+  /** @type {number} Минимальное время блокировки интерфейса в миллисекундах */
   #upperLimit;
+
+  /** @type {HTMLElement|null} Элемент, блокирующий интерфейс */
   #element;
+
+  /** @type {number} Время вызова метода block */
   #startTime;
+
+  /** @type {number} Время вызова метода unblock */
   #endTime;
+
+  /** @type {number} Идентификатор таймера */
   #timerId;
 
+  /**
+   * @param {Object} config Объект с настройками блокировщика
+   * @param {number} config.lowerLimit Время до блокировки интерфейса в миллисекундах. Если вызвать метод unblock раньше, то интерфейс заблокирован не будет
+   * @param {number} config.upperLimit Минимальное время блокировки в миллисекундах. Минимальная длительность блокировки
+   */
   constructor({lowerLimit, upperLimit}) {
     this.#lowerLimit = lowerLimit;
     this.#upperLimit = upperLimit;
@@ -17,6 +36,7 @@ export default class UiBlocker {
     document.body.append(this.#element);
   }
 
+  /** Метод для блокировки интерфейса */
   block() {
     this.#startTime = Date.now();
     this.#timerId = setTimeout(() => {
@@ -24,6 +44,7 @@ export default class UiBlocker {
     }, this.#lowerLimit);
   }
 
+  /** Метод для разблокировки интерфейса */
   unblock() {
     this.#endTime = Date.now();
     const duration = this.#endTime - this.#startTime;
@@ -41,11 +62,13 @@ export default class UiBlocker {
     setTimeout(this.#disactivateBlocking, this.#upperLimit - duration);
   }
 
+  /** Метод, добавляющий CSS-класс и обработчик */
   #activateBlocking = () => {
     this.#element.classList.add('ui-blocker--on');
     document.addEventListener('keydown', this.#documentKeydownHandler);
   };
 
+  /** Метод, убирающий CSS-класс и обработчик */
   #disactivateBlocking = () => {
     this.#element.classList.remove('ui-blocker--on');
     document.removeEventListener('keydown', this.#documentKeydownHandler);
